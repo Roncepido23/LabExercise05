2aed4e583d25e40b82326b0ca8b9ffa8b1767e73
Roncepido23 <oncepidorobin34@gmail.com>
2025-10-26 23:22:04 -0700

    Lab malapit na

---
+++ FrmRegistration.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace ReadingFile
+{
+    partial class FrmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRegistration));
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            tbStudentNo = new TextBox();
+            tbLastName = new TextBox();
+            tbAge = new TextBox();
+            tbFirstName = new TextBox();
+            tbMiddle = new TextBox();
+            dtpBirthday = new DateTimePicker();
+            cbProgram = new ComboBox();
+            label10 = new Label();
+            cbGender = new ComboBox();
+            tbContactNo = new TextBox();
+            btnRegister = new Button();
+            btnRecords = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Impact", 29F);
+            label1.ForeColor = Color.DarkSlateGray;
+            label1.Location = new Point(24, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(217, 47);
+            label1.TabIndex = 0;
+            label1.Text = "Registration";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Impact", 14F);
+            label2.ForeColor = Color.DarkSlateGray;
+            label2.Location = new Point(43, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 23);
+            label2.TabIndex = 1;
+            label2.Text = "Student No.";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Impact", 14F);
+            label3.ForeColor = Color.DarkSlateGray;
+            label3.Location = new Point(43, 191);
+            label3.Name = "label3";
+            label3.Size = new Size(90, 23);
+            label3.TabIndex = 2;
+            label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Impact", 14F);
+            label4.ForeColor = Color.DarkSlateGray;
+            label4.Location = new Point(417, 260);
+            label4.Name = "label4";
+            label4.Size = new Size(39, 23);
+            label4.TabIndex = 3;
+            label4.Text = "Age";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Impact", 14F);
+            label5.ForeColor = Color.DarkSlateGray;
+            label5.Location = new Point(29, 260);
+            label5.Name = "label5";
+            label5.Size = new Size(78, 23);
+            label5.TabIndex = 4;
+            label5.Text = "Birthday";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Impact", 14F);
+            label6.ForeColor = Color.DarkSlateGray;
+            label6.Location = new Point(378, 119);
+            label6.Name = "label6";
+            label6.Size = new Size(79, 23);
+            label6.TabIndex = 5;
+            label6.Text = "Program";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Impact", 14F);
+            label7.ForeColor = Color.DarkSlateGray;
+            label7.Location = new Point(364, 191);
+            label7.Name = "label7";
+            label7.Size = new Size(93, 23);
+            label7.TabIndex = 6;
+            label7.Text = "First Name";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.BackColor = Color.Transparent;
+            label8.Font = new Font("Impact", 14F);
+            label8.ForeColor = Color.DarkSlateGray;
+            label8.Location = new Point(723, 194);
+            label8.Name = "label8";
+            label8.Size = new Size(37, 23);
+            label8.TabIndex = 7;
+            label8.Text = "M.I.";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.BackColor = Color.Transparent;
+            label9.Font = new Font("Impact", 14F);
+            label9.ForeColor = Color.DarkSlateGray;
+            label9.Location = new Point(651, 122);
+            label9.Name = "label9";
+            label9.Size = new Size(99, 23);
+            label9.TabIndex = 8;
+            label9.Text = "Contact No.";
+            // 
+            // tbStudentNo
+            // 
+            tbStudentNo.Location = new Point(153, 124);
+            tbStudentNo.Name = "tbStudentNo";
+            tbStudentNo.Size = new Size(100, 23);
+            tbStudentNo.TabIndex = 9;
+            // 
+            // tbLastName
+            // 
+            tbLastName.Location = new Point(153, 191);
+            tbLastName.Name = "tbLastName";
+            tbLastName.Size = new Size(100, 23);
+            tbLastName.TabIndex = 10;
+            // 
+            // tbAge
+            // 
+            tbAge.Location = new Point(472, 265);
+            tbAge.Name = "tbAge";
+            tbAge.Size = new Size(78, 23);
+            tbAge.TabIndex = 11;
+            // 
+            // tbFirstName
+            // 
+            tbFirstName.Location = new Point(472, 191);
+            tbFirstName.Name = "tbFirstName";
+            tbFirstName.Size = new Size(121, 23);
+            tbFirstName.TabIndex = 12;
+            // 
+            // tbMiddle
+            // 
+            tbMiddle.Location = new Point(771, 191);
+            tbMiddle.Name = "tbMiddle";
+            tbMiddle.Size = new Size(121, 23);
+            tbMiddle.TabIndex = 14;
+            // 
+            // dtpBirthday
+            // 
+            dtpBirthday.Location = new Point(121, 260);
+            dtpBirthday.Name = "dtpBirthday";
+            dtpBirthday.Size = new Size(200, 23);
+            dtpBirthday.TabIndex = 15;
+            // 
+            // cbProgram
+            // 
+            cbProgram.FormattingEnabled = true;
+            cbProgram.Location = new Point(472, 122);
+            cbProgram.Name = "cbProgram";
+            cbProgram.Size = new Size(121, 23);
+            cbProgram.TabIndex = 16;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = Color.Transparent;
+            label10.Font = new Font("Impact", 14F);
+            label10.ForeColor = Color.DarkSlateGray;
+            label10.Location = new Point(683, 265);
+            label10.Name = "label10";
+            label10.Size = new Size(67, 23);
+            label10.TabIndex = 17;
+            label10.Text = "Gender";
+            label10.Click += label10_Click;
+            // 
+            // cbGender
+            // 
+            cbGender.FormattingEnabled = true;
+            cbGender.Location = new Point(759, 265);
+            cbGender.Name = "cbGender";
+            cbGender.Size = new Size(121, 23);
+            cbGender.TabIndex = 18;
+            // 
+            // tbContactNo
+            // 
+            tbContactNo.Location = new Point(771, 122);
+            tbContactNo.Name = "tbContactNo";
+            tbContactNo.Size = new Size(121, 23);
+            tbContactNo.TabIndex = 19;
+            // 
+            // btnRegister
+            // 
+            btnRegister.BackColor = Color.DodgerBlue;
+            btnRegister.FlatStyle = FlatStyle.Flat;
+            btnRegister.Font = new Font("Impact", 12F);
+            btnRegister.ForeColor = SystemColors.ButtonFace;
+            btnRegister.Location = new Point(275, 358);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(127, 39);
+            btnRegister.TabIndex = 20;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = false;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // btnRecords
+            // 
+            btnRecords.BackColor = Color.DodgerBlue;
+            btnRecords.FlatStyle = FlatStyle.Flat;
+            btnRecords.Font = new Font("Impact", 12F);
+            btnRecords.ForeColor = SystemColors.ButtonFace;
+            btnRecords.Location = new Point(456, 358);
+            btnRecords.Name = "btnRecords";
+            btnRecords.Size = new Size(125, 39);
+            btnRecords.TabIndex = 21;
+            btnRecords.Text = "Records";
+            btnRecords.UseVisualStyleBackColor = false;
+            btnRecords.Click += btnRecords_Click;
+            // 
+            // FrmRegistration
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(924, 436);
+            Controls.Add(btnRecords);
+            Controls.Add(btnRegister);
+            Controls.Add(tbContactNo);
+            Controls.Add(cbGender);
+            Controls.Add(label10);
+            Controls.Add(cbProgram);
+            Controls.Add(dtpBirthday);
+            Controls.Add(tbMiddle);
+            Controls.Add(tbFirstName);
+            Controls.Add(tbAge);
+            Controls.Add(tbLastName);
+            Controls.Add(tbStudentNo);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "FrmRegistration";
+            Text = "FrmRegistration";
+            Load += FrmRegistration_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox tbStudentNo;
+        private TextBox tbLastName;
+        private TextBox tbAge;
+        private TextBox tbFirstName;
+        private TextBox tbMiddle;
+        private DateTimePicker dtpBirthday;
+        private ComboBox cbProgram;
+        private Label label10;
+        private ComboBox cbGender;
+        private TextBox tbContactNo;
+        private Button btnRegister;
+        private Button btnRecords;
+    }
+}
---
+++ FrmStudentRecord.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ReadingFile
+{
+    partial class FrmStudentRecord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lvShowText = new ListView();
+            btnRegister = new Button();
+            btnFind = new Button();
+            btnUpload = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Impact", 24F);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(19, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(177, 39);
+            label1.TabIndex = 0;
+            label1.Text = "View Record";
+            // 
+            // lvShowText
+            // 
+            lvShowText.Location = new Point(19, 75);
+            lvShowText.Name = "lvShowText";
+            lvShowText.Size = new Size(582, 264);
+            lvShowText.TabIndex = 1;
+            lvShowText.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnRegister
+            // 
+            btnRegister.BackColor = Color.Black;
+            btnRegister.FlatStyle = FlatStyle.Flat;
+            btnRegister.Font = new Font("Impact", 12F);
+            btnRegister.ForeColor = SystemColors.ButtonFace;
+            btnRegister.Location = new Point(442, 357);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(99, 41);
+            btnRegister.TabIndex = 2;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = false;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // btnFind
+            // 
+            btnFind.BackColor = Color.Black;
+            btnFind.FlatStyle = FlatStyle.Flat;
+            btnFind.Font = new Font("Impact", 12F);
+            btnFind.ForeColor = SystemColors.ButtonFace;
+            btnFind.Location = new Point(265, 358);
+            btnFind.Name = "btnFind";
+            btnFind.Size = new Size(99, 40);
+            btnFind.TabIndex = 3;
+            btnFind.Text = "Find";
+            btnFind.UseVisualStyleBackColor = false;
+            btnFind.Click += btnFind_Click;
+            // 
+            // btnUpload
+            // 
+            btnUpload.BackColor = Color.Black;
+            btnUpload.FlatStyle = FlatStyle.Flat;
+            btnUpload.Font = new Font("Impact", 12F);
+            btnUpload.ForeColor = SystemColors.ButtonFace;
+            btnUpload.Location = new Point(65, 361);
+            btnUpload.Name = "btnUpload";
+            btnUpload.Size = new Size(99, 37);
+            btnUpload.TabIndex = 4;
+            btnUpload.Text = "Upload";
+            btnUpload.UseVisualStyleBackColor = false;
+            btnUpload.Click += btnUpload_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FrmStudentRecord
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.IndianRed;
+            ClientSize = new Size(619, 422);
+            Controls.Add(btnUpload);
+            Controls.Add(btnFind);
+            Controls.Add(btnRegister);
+            Controls.Add(lvShowText);
+            Controls.Add(label1);
+            Name = "FrmStudentRecord";
+            Text = "FrmStudentRecord";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListView lvShowText;
+        private Button btnRegister;
+        private Button btnFind;
+        private Button btnUpload;
+        private OpenFileDialog openFileDialog1;
+    }
+}
